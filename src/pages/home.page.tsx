4b45f0bfb2b7a8061f65bb54fa7486543441913b
Grigor Aleksanyan <grigdevelop@gmail.com>
2021-01-01 20:12:50 +0100

    datepicker improve

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,3 +1,4 @@
+import { concat, slice } from "lodash";
 import { useState } from "react";
 import { useModal } from "../utils/universalModal";
 
@@ -7,73 +8,121 @@ const showCalendar = (calendar: []) => {
     }
 }
 
-const getMonth = (year: number, month: number) => {
-    // months are working on 0 based system
-    month = month - 1;
-
-    // create dates
-    const lastDate = new Date(year, month + 1, 0);
-    const firstDate = new Date(year, month, 1);
-    const prevMonthLastDay = new Date(year, month, 0);
-    const nextMonthFirstDay = new Date(year, month + 1, 1);
-
-    // last date info
-    const lastDayInMonth = lastDate.getDate();
-    const firstDayOfWeekInMonth = firstDate.getDay();
-    console.log("firstDayOfWeekInMonth", firstDate, firstDayOfWeekInMonth);
-
-    const monthInfo = [];
-
-    if (firstDayOfWeekInMonth > 0) {
-        const prevLastDay = prevMonthLastDay.getDate();
-        console.log("prevLastDay", prevMonthLastDay);
-        for (let i = prevLastDay - firstDayOfWeekInMonth + 1; i <= prevLastDay; i++) {
-            monthInfo.push({
-                day: i,
-                month: prevMonthLastDay.getMonth(),
-                year: prevMonthLastDay.getFullYear()
-            })
-        }
-    }
-
-    for (let i = 1; i <= lastDayInMonth; i++) {
-        monthInfo.push({
-            day: i,
-            month,
-            year
-        });
-    }
-
-    for (let i = 1; i < (7 * 6) - monthInfo.length; i++) {
-        monthInfo.push({
-            day: i,
-            month: month + 1,
-            year: nextMonthFirstDay.getFullYear()
-        })
-    }
-
-    console.log("full year", monthInfo);
+type DayInfo = {
+    year: number,
+    month: number,
+    day: number,
+    dayOfWeek: number
 };
-getMonth(2021, 1);
+
+function getMonthInfo(current: Date, starts: number = 1, ends?: number | undefined): DayInfo[] {
+    console.log("I'm here");
+    const year = current.getFullYear(),
+        month = current.getMonth(),
+        lastDay = ends ?? new Date(year, month + 1, 0).getDate();
+    let dayOfWeek = new Date(year, month, 1).getDay();
+
+    if (starts <= 0) {
+        starts = lastDay - starts;
+    }
+
+    const monthInfo: DayInfo[] = [];
+
+    for (let day = starts; day <= lastDay; day++) {
+        monthInfo.push({
+            year, month, day, dayOfWeek
+        });
+        dayOfWeek = (dayOfWeek + 1) % 7;
+    }
+
+    return monthInfo;
+}
+
+const btnStyle: React.CSSProperties = {
+    width: 30,
+    padding: "2px 0px",
+    fontSize: 12,
+    flex: "none"
+};
+
+const getMonthCalendarInfo = (current: Date) => {
+    const year = current.getFullYear(), month = current.getMonth();
+    return concat(getMonthInfo(new Date(year, month - 1)), getMonthInfo(current), getMonthInfo(new Date(year, month + 1)));
+};
+
+const CalendarPicker = ({ year, month }: { year: number, month: number }) => {
+    let selectedYear = year,
+        selectedMonth = month;
+    let calenderInfo = getMonthCalendarInfo(new Date(selectedYear, selectedMonth));
+    calenderInfo = slice(calenderInfo, 7 * 4, 7 * 4 + 7 * 6);
+
+    const getKey = (dayInfo: DayInfo): string => {
+        return `${dayInfo.month} - ${dayInfo.day}`;
+    };
+
+    return (
+        <>
+            {calenderInfo.map(dayInfo => <button key={getKey(dayInfo)} disabled={dayInfo.month !== selectedMonth} className="btn btn-sm" style={btnStyle}>{dayInfo.day}</button>)}
+        </>
+    );
+}
+
+
+const CalendarPanel = () => {
+    const [year, setYear] = useState(2021);
+    const [month, setMonth] = useState(0);
+
+    const nextMonth = () => {
+        if (month === 11) {
+            setYear(year + 1);
+            setMonth(0);
+        } else {
+            setMonth(month + 1);
+        }
+    };
+
+    const prevMonth = () => {
+        if (month === 0) {
+            setYear(year - 1);
+            setMonth(11);
+        } else {
+            setMonth(month - 1);
+        }
+    };
+
+    const daysOfWeek = [
+        "Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"
+    ];
+
+    return (
+        <>
+            <div className="row">
+                <div className="col">
+                    {year}
+                </div>
+                <div className="col">
+                    <div className="btn-group">
+                        <button onClick={prevMonth} className="btn btn-sm">{"<"}</button>
+                        <button onClick={nextMonth} className="btn btn-sm">{">"}</button>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div className="btn-group d-flex flex-wrap align-content-start" role="group" aria-label="First group" style={{ width: 210 }}>
+                {daysOfWeek.map(dof => <span className="btn-sm text-center" style={btnStyle}>{dof}</span>)}
+                <hr />
+                <CalendarPicker year={year} month={month} />
+            </div>
+        </>
+    );
+}
 
 
 export const HomePage = () => (
     <>
         <div className="row">
             <div className="col-2">
-                <div className="btn-group" role="group" aria-label="First group">
-                    <button type="button" className="btn btn-sm">1</button>
-                    <button type="button" className="btn btn-secondary btn-sm">2</button>
-                    <button type="button" className="btn btn-secondary btn-sm">3</button>
-                    <button type="button" className="btn btn-secondary btn-sm">4</button>
-                </div>
-                <div className="btn-group" role="group" aria-label="First group">
-                    <button type="button" className="btn btn-secondary btn-sm">1</button>
-                    <button type="button" className="btn btn-secondary btn-sm">2</button>
-                    <button type="button" className="btn btn-secondary btn-sm">32</button>
-                    <button type="button" className="btn btn-secondary btn-sm">4</button>
-                </div>
-
+                <CalendarPanel />
             </div>
         </div>
     </>
